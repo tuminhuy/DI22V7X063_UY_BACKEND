cd8675b8b627c8a37f5e75e496c038f6cf0560f8
tuminhuy <tuminhuy21@gmail.com>
2023-11-21 21:55:54 +0700

    lan1

--- Backend-1/app/services/contact.service.js
+++ Backend-1/app/services/contact.service.js
@@ -3,5 +3,30 @@ class ContactService {
     construtor(client){
         this.Contact = client.db().collection("contacts");
     }
+    // dinh nghia cac phuong thuc truy xuat csdl su dung mongodb api
+    extractConactData(payload) {
+        const contact = {
+name: payload.name,
+email: payload.email,
+address: payload.address,
+phone: payload.phone,
+favorite: payload.favorite,
+};
+// Remove undefined fields
+Object.keys(contact).forEach(
+(key) => contact[key] === undefined && delete contact[key]
+);
+return contact;
 }
+async create(payload) {
+const contact = this.extractConactData(payload);
+const result = await this.Contact.findOneAndUpdate(
+contact,
+{ $set: { favorite: contact.favorite === true } },
+{ returnDocument: "after", upsert: true }
+);
+return result;
+}
+}
+
 module.exports = ContactService;
